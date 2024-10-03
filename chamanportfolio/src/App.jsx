441545701165a8d4eb38d6bebf38a233628919ff
Chaman Betrabet <betrabet.c@northeastern.edu>
2024-10-03 12:56:01 -0400

    Changed Hero section, added three js node modules, added HackerRoom section - need to work on 3d model additions

--- chamanportfolio/src/App.jsx
+++ chamanportfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
 
 const App = () => {
   return (
     <main className="container mx-auto px-4 sm:px-6 lg:px-8">
       <Navbar/>
+      <Hero/>
     </main>
   )
 }
---
+++ chamanportfolio/src/sections/Hero.jsx
@@ -0,0 +1,24 @@
+import { Canvas } from "@react-three/fiber"
+import { PerspectiveCamera } from "@react-three/drei"
+import HackerRoom from "../components/HackerRoom"
+
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative" id="home">
+        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+            <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+                Hi, I am Chaman <span className="waving-hand">👋🏻</span>
+            </p>
+            <p className="hero_tag text-gray_gradient">Crafting Scalable Solutions & Empowering Digital Experiences</p>
+        </div>
+        <div className="w-full h-full absolute inset-0">
+            <Canvas className="w-full h-full">
+                <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
+                <HackerRoom/>
+            </Canvas>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
