fe7452f621a4b0d33f95d7af815c573564e6cdc6
Chaman Betrabet <betrabet.c@northeastern.edu>
2024-10-04 00:30:13 -0400

    worked on hero section

--- chamanportfolio/src/sections/Hero.jsx
+++ chamanportfolio/src/sections/Hero.jsx
@@ -1,20 +1,72 @@
 import { Canvas } from "@react-three/fiber"
 import { PerspectiveCamera } from "@react-three/drei"
-import HackerRoom from "../components/HackerRoom"
+import { HackerRoom } from "../components/HackerRoom"
+import { Suspense } from "react"
+import CanvasLoader from "../components/CanvasLoader"
+import { Leva, useControls } from "leva"
+
 
 const Hero = () => {
+  const x = useControls('HackerRoom', {
+    positionX: {
+        value: 2.5,
+        min: -10,
+        max: 10
+    },
+    positionY: {
+        value: 2.5,
+        min: -12,
+        max: 10
+    },
+    positionZ: {
+        value: 2.5,
+        min: -10,
+        max: 10
+    },
+    rotationX: {
+        value: 0,
+        min: -10,
+        max: 10
+    },
+    rotationY: {
+        value: 0,
+        min: -10,
+        max: 10
+    },
+    rotationZ: {
+        value: 0,
+        min: -10,
+        max: 10
+    },
+    scale: {
+        value: 1,
+        min: 0.1,
+        max: 10
+    }
+  })
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
-        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <div className="relative w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
             <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
                 Hi, I am Chaman <span className="waving-hand">👋🏻</span>
             </p>
             <p className="hero_tag text-gray_gradient">Crafting Scalable Solutions & Empowering Digital Experiences</p>
         </div>
-        <div className="w-full h-full absolute inset-0">
+         <div className="w-full h-full absolute inset-0">
+            <Leva/>
             <Canvas className="w-full h-full">
+                <Suspense fallback={<CanvasLoader/>}>
                 <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
-                <HackerRoom/>
+                <HackerRoom 
+                // scale={0.07} 
+                //position={[0, 0 , 0]} 
+                //rotation={[0, -Math.PI / 2, 0]}
+                position={[-0.7, -12, -1.9]}
+                rotation={[0, -Math.PI, 0]}
+                scale={0.1}/>
+                <ambientLight intensity={1}/>
+                <directionalLight position={[10, 10, 10]} intensity={0.5}/>
+                </Suspense>
             </Canvas>
         </div>
     </section>
